bd6befa1a9076f7626bece0d4b3256f1b069fc30
Matt Eland <Matt.Eland@GMail.com>
2025-03-30 00:56:26 -0400

    Add WorldService with player, level management, and visibility
    This commit introduces the `WorldService` class for managing player creation, level initialization, and calculating the visible objects and tiles in a viewport. Unit tests are added to validate the service's behavior, ensuring reliability for core game functionalities.

--- MattEland.Emergence.Tests/GlobalUsings.cs
+++ MattEland.Emergence.Tests/GlobalUsings.cs
@@ -1,5 +1,6 @@
 global using JetBrains.Annotations;
 global using MattEland.Emergence.DesktopClient.Helpers;
 global using MattEland.Emergence.World.Models;
+global using MattEland.Emergence.World.Services;
 global using Microsoft.Xna.Framework;
 global using Shouldly;
---
+++ MattEland.Emergence.World/Services/WorldService.cs
@@ -0,0 +1,60 @@
+using MattEland.Emergence.World.Models;
+
+namespace MattEland.Emergence.World.Services;
+
+public class WorldService : IWorldService
+{
+    public ViewportData GetVisibleObjects(Player perspective, Level level, ViewportDimensions viewport)
+    {
+        // Figure out upper left and lower right corners of the viewport
+        int halfViewportWidth = viewport.Width / 2;
+        int halfViewportHeight = viewport.Height / 2;
+        WorldPos upperLeft = new(perspective.Pos.X - halfViewportWidth, perspective.Pos.Y - halfViewportHeight);
+        WorldPos lowerRight = upperLeft.Offset(viewport.Width - 1, viewport.Height - 1);
+
+        return new ViewportData
+        {
+            Viewport = viewport,
+            Center = perspective.Pos,
+            VisibleTiles = PopulateVisibleTiles(level, viewport, upperLeft, lowerRight),
+            VisibleObjects = level.Objects.Where(obj => IsInViewport(obj.Pos, upperLeft, lowerRight))
+        };
+    }
+
+
+    public Player CreatePlayer() => new(new WorldPos(0, 0));
+
+    public Level CreateLevel(Player player)
+    {
+        Level level = new(30, 30);
+        player.Pos = new WorldPos(2, 3);
+        level.AddObject(player);
+
+        return level;
+    }
+
+    private static List<TileInfo> PopulateVisibleTiles(Level level, ViewportDimensions viewport, WorldPos upperLeft,
+        WorldPos lowerRight)
+    {
+        List<TileInfo> visibleTiles = new(viewport.Width * viewport.Height);
+
+        for (int y = upperLeft.Y; y <= lowerRight.Y; y++)
+        {
+            for (int x = upperLeft.X; x <= lowerRight.X; x++)
+            {
+                WorldPos pos = new(x, y);
+                visibleTiles.Add(new TileInfo
+                {
+                    Pos = pos,
+                    Floor = level.GetFloorInfo(pos)
+                });
+            }
+        }
+
+        return visibleTiles;
+    }
+
+    private static bool IsInViewport(WorldPos pos, WorldPos upperLeft, WorldPos lowerRight) =>
+        pos.X >= upperLeft.X && pos.X <= lowerRight.X &&
+        pos.Y >= upperLeft.Y && pos.Y <= lowerRight.Y;
+}
