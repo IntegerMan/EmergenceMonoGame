f26cf994904a17dd5907fe8e7894eacb9c9b2e3f
Matt Eland <Matt.Eland@GMail.com>
2025-03-30 00:05:01 -0400

    Better test style.

--- MattEland.Emergence.Tests/Helpers/ColorHelpersTests.cs
+++ MattEland.Emergence.Tests/Helpers/ColorHelpersTests.cs
@@ -6,32 +6,52 @@ public class ColorHelpersTests
     [Fact]
     public void GetTileColor_ReturnsBlack_ForVoidFloor()
     {
+        // Arrange
         TileInfo tile = new() { Floor = FloorType.Void };
+
+        // Act
         Color color = tile.GetTileColor();
+
+        // Assert
         color.ShouldBe(Color.Black);
     }
 
     [Fact]
     public void GetTileColor_ReturnsGray_ForNormalFloor()
     {
+        // Arrange
         TileInfo tile = new() { Floor = FloorType.Normal };
+
+        // Act
         Color color = tile.GetTileColor();
+
+        // Assert
         color.ShouldBe(Color.Gray);
     }
 
     [Fact]
     public void GetTileColor_ReturnsMagenta_ForVirusFloor()
     {
+        // Arrange
         TileInfo tile = new() { Floor = FloorType.Virus };
+
+        // Act
         Color color = tile.GetTileColor();
+
+        // Assert
         color.ShouldBe(Color.Magenta);
     }
 
     [Fact]
     public void GetTileColor_ReturnsDimGray_ForWildernessFloor()
     {
+        // Arrange
         TileInfo tile = new() { Floor = FloorType.Wilderness };
+
+        // Act
         Color color = tile.GetTileColor();
+
+        // Assert
         color.ShouldBe(Color.DimGray);
     }
 
@@ -40,7 +60,10 @@ public class ColorHelpersTests
     {
         Should.Throw<NotSupportedException>(() =>
         {
+            // Arrange
             TileInfo tile = new() { Floor = (FloorType)999 };
+
+            // Act
             tile.GetTileColor();
         });
     }
--- MattEland.Emergence.Tests/Helpers/ScreenHelpersTests.cs
+++ MattEland.Emergence.Tests/Helpers/ScreenHelpersTests.cs
@@ -8,28 +8,41 @@ public class ScreenHelpersTests
     [Fact]
     public void ToRectangle_ReturnsCorrectRectangle_ForPositiveCoordinates()
     {
+        // Arrange
         WorldPos pos = new WorldPos(2, 3);
+
+        // Act
         Rectangle rect = pos.ToRectangle();
-        Assert.Equal(
-            new Rectangle(2 * EmergenceGame.TileSize, 3 * EmergenceGame.TileSize, EmergenceGame.TileSize,
-                EmergenceGame.TileSize), rect);
+
+        // Assert
+        rect.ShouldBe(new Rectangle(2 * EmergenceGame.TileSize, 3 * EmergenceGame.TileSize, EmergenceGame.TileSize,
+            EmergenceGame.TileSize));
     }
 
     [Fact]
     public void ToRectangle_ReturnsCorrectRectangle_ForNegativeCoordinates()
     {
+        // Arrange
         WorldPos pos = new WorldPos(-1, -2);
+
+        // Act
         Rectangle rect = pos.ToRectangle();
-        Assert.Equal(
-            new Rectangle(-1 * EmergenceGame.TileSize, -2 * EmergenceGame.TileSize, EmergenceGame.TileSize,
-                EmergenceGame.TileSize), rect);
+
+        // Assert
+        rect.ShouldBe(new Rectangle(-1 * EmergenceGame.TileSize, -2 * EmergenceGame.TileSize, EmergenceGame.TileSize,
+            EmergenceGame.TileSize));
     }
 
     [Fact]
     public void ToRectangle_ReturnsCorrectRectangle_ForZeroCoordinates()
     {
+        // Arrange
         WorldPos pos = new WorldPos(0, 0);
+
+        // Act
         Rectangle rect = pos.ToRectangle();
-        Assert.Equal(new Rectangle(0, 0, EmergenceGame.TileSize, EmergenceGame.TileSize), rect);
+
+        // Assert
+        rect.ShouldBe(new Rectangle(0, 0, EmergenceGame.TileSize, EmergenceGame.TileSize));
     }
 }
